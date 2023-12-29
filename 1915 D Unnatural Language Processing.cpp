efd172a44614d853116f421a38bf3b785ff951c2
Pro Donkey <153301727+pro-donkey@users.noreply.github.com>
2023-12-29 08:14:12 +0530

    Create 1915 D Unnatural Language Processing.cpp

---
+++ 1915 D Unnatural Language Processing.cpp
@@ -0,0 +1,81 @@
+ 
+ 
+/*
+ 
+    Author :: Bully Butcher
+ 
+ */
+ 
+#include <bits/stdc++.h>
+#define pb push_back
+#define popb pop_back
+#define print(a)          \
+    for (auto i : a)      \
+    {                     \
+        cout << i << ' '; \
+    }                     \
+    cout << endl;
+typedef long long ll;
+using namespace std;
+ 
+typedef vector<int> vin;
+typedef vector<string> vst;
+typedef vector<char> vch;
+typedef vector<ll> vl;
+typedef set<int> si;
+typedef map<int, int> mii;
+typedef unsigned long long ull;
+#define f first
+#define s second
+#define MOD 1000000007
+#define gcd(a, b) __gcd(a, b)
+#define lcm(a, b) (a * (b / gcd(a, b)))
+#define all(a) (a).begin(), (a).end()
+ 
+void solve()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+        string s;
+        cin >> s;
+        if (n == 1)
+            cout << s << endl;
+        else
+        {
+            string ans = "";
+            for (int i = n - 1; i > 0;)
+            {
+                if (s[i] == 'b' || s[i] == 'c' || s[i] == 'd')
+                {
+                    ans += s[i];
+                    ans += s[i - 1];
+                    ans += s[i - 2];
+                    ans += '.';
+                    i = i - 3;
+                }
+                else
+                {
+                    ans += s[i];
+                    ans += s[i - 1];
+                    ans += '.';
+                    i = i - 2;
+                }
+            }
+            ans.pop_back();
+            reverse(ans.begin(), ans.end());
+            cout << ans << endl;
+        }
+    }
+}
+ 
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    solve();
+}
