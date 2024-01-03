740408acce26874eb6b27a0fc1e69088301b16b6
Pro Donkey <153301727+pro-donkey@users.noreply.github.com>
2024-01-03 15:47:12 +0530

    Create JOE is on TV!.cpp

---
+++ JOE is on TV!.cpp
@@ -0,0 +1,58 @@
+
+
+/*
+
+    Author :: Programming  Donkey  :)
+I am here to improve my coding skills i give up easy plz help me i get stuck in a problem very easy plz help me plz help me
+I think i am going to be poor plz help mee i have very bad financial condition i cant code plz help me
+
+ */
+
+#include <bits/stdc++.h>
+#define pb push_back
+#define popb pop_back
+#define mp make_pair
+#define ff first
+#define ss second
+#define PI 3.141592653589793238462
+#define set_bits __builtin_popcountll
+#define print(a)          \
+    for (auto i : a)      \
+    {                     \
+        cout << i << ' '; \
+    }                     \
+    cout << endl;
+#define eb emplace_back
+#define mt make_tuple
+#define MOD 1000000007
+#define MOD1 998244353
+
+typedef long long ll;
+typedef long long int llt;
+using namespace std;
+
+typedef vector<int> vin;
+typedef vector<string> vst;
+typedef vector<char> vch;
+
+void solve()
+{
+    int n;
+    cin >> n;
+    double sum = 0;
+    for (int i = n; i >= 1; i--)
+    {
+        sum += 1.0 / i;
+    }
+    cout << fixed << setprecision(12);
+    ;
+    cout << sum << endl;
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    solve();
+}
