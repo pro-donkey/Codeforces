28b3c5cc6e300ae8a7620769816f9190459d6581
Pro Donkey <153301727+pro-donkey@users.noreply.github.com>
2024-01-04 17:53:42 +0530

    Create 405A. Gravity Flip.cpp

---
+++ 405A. Gravity Flip.cpp
@@ -0,0 +1,60 @@
+
+
+/*
+
+    Author :: Programming  Donkey  :)
+I am here to improve my coding skills i give up easy plz help me i get stuck in a problem very easy plz help me plz help me
+I think i am going to be poor plz help mee i have very bad financial condition i cant code plz help me
+
+ */
+
+#include <bits/stdc++.h>
+#define pb push_back
+#define popb pop_back
+#define mp make_pair
+#define ff first
+#define ss second
+#define PI 3.141592653589793238462
+#define set_bits __builtin_popcountll
+#define print(a)          \
+    for (auto i : a)      \
+    {                     \
+        cout << i << ' '; \
+    }                     \
+    cout << endl;
+#define eb emplace_back
+#define mt make_tuple
+#define MOD 1000000007
+#define MOD1 998244353
+
+typedef long long ll;
+typedef long long int llt;
+using namespace std;
+
+typedef vector<int> vin;
+typedef vector<string> vst;
+typedef vector<char> vch;
+
+void solve()
+{
+    int n;
+    cin >> n;
+    int arr[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+    sort(arr, arr + n);
+    for (int i : arr)
+    {
+        cout << i << " ";
+    }
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    solve();
+}
