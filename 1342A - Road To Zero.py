e5bb01b0bdc0d22a95030e4733ced5ec123ddf5b
Pro Donkey <153301727+pro-donkey@users.noreply.github.com>
2025-01-13 14:14:28 +0530

    Create 1342A - Road To Zero.py

---
+++ 1342A - Road To Zero.py
@@ -0,0 +1,106 @@
+import sys
+import os
+import math
+import random
+from collections import Counter, defaultdict, deque
+from functools import lru_cache, reduce
+from itertools import accumulate, combinations, permutations
+from heapq import nsmallest, nlargest, heapify, heappop, heappush
+import bisect
+from io import BytesIO
+BUFSIZE = 8192  
+
+class FastIO:
+    def __init__(self, file):
+        self._fd = file.fileno()
+        self.buffer = BytesIO()
+        self.newlines = 0  # Initialize the `newlines` attribute
+        self.writable = "x" in file.mode or "r" not in file.mode
+        self.write = self.buffer.write if self.writable else None
+
+    def read(self):
+        while True:
+            b = os.read(self._fd, BUFSIZE)
+            if not b:
+                break
+            self.buffer.seek(0, 2), self.buffer.write(b), self.buffer.seek(0)
+        return self.buffer.read()
+
+    def readline(self):
+        while self.newlines == 0:
+            b = os.read(self._fd, BUFSIZE)
+            self.newlines = b.count(b"\n") + (not b)
+            ptr = self.buffer.tell()
+            self.buffer.seek(0, 2), self.buffer.write(b), self.buffer.seek(ptr)
+        self.newlines -= 1
+        return self.buffer.readline()
+
+    def flush(self):
+        if self.writable:
+            os.write(self._fd, self.buffer.getvalue())
+            self.buffer.truncate(0), self.buffer.seek(0)
+
+
+class IOWrapper:
+    def __init__(self, file):
+        self.buffer = FastIO(file)
+        self.flush = self.buffer.flush
+        self.writable = self.buffer.writable
+        self.write = lambda s: self.buffer.write(s.encode("ascii"))
+        self.read = lambda: self.buffer.read().decode("ascii")
+        self.readline = lambda: self.buffer.readline().decode("ascii")
+
+# Replacing standard input and output for fast I/O
+sys.stdin, sys.stdout = IOWrapper(sys.stdin), IOWrapper(sys.stdout)
+
+def input(): return sys.stdin.readline().strip()
+
+def II():
+    return int(input())
+
+def MI():
+    return map(int, input().split())
+
+def LII():
+    return list(map(int, input().split()))
+
+def SI():
+    return set(map(int, input().split()))
+
+def GMI():
+    return map(lambda x: int(x) - 1, input().split())
+
+def LGMI():
+    return list(map(lambda x: int(x) - 1, input().split()))
+
+def YESNO(cond):
+    print("YES" if cond else "NO")
+
+def FACTORIAL(n):
+    return math.factorial(n)
+
+def GCD(a, b):
+    while b:
+        a, b = b, a % b
+    return a
+
+def LCM(a, b):
+    return a * b // GCD(a, b)
+
+
+if __name__ == "__main__":
+    t = II()
+    for _ in range(t):
+        x,y = MI()
+        a,b = MI()
+        b = min(2*a,b)
+        
+        if x < y:
+            x,y= y,x 
+        print(y*b + (x-y)*a)
+
+
+         
+        
+
+    
